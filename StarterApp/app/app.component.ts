c561cea93e274f46cd6e5af0e890ea13e7160bd2
xmano <mano@xkanda.com>
2017-02-13 22:15:52 +0800

    Initial version.

---
+++ StarterApp/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello, World!',
+})
+
+export class AppComponent {
+  public name;
+  constructor(){
+    //this.name = "Angular 2 Rocks!";
+  }  
+}
